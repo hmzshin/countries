5754d2d92edce1c44b749f99651110e4a7c6eb68
hmzshin <hamzasahin@outlook.com>
2024-02-11 18:12:54 +0300

    refactor: Add styling to Content component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -6,7 +6,7 @@ const Content = () => {
   const { countries } = useContext(CountriesContextObject);
 
   return (
-    <section className="flex flex-col items-center justify-center gap-2">
+    <section className="flex flex-col items-center justify-center gap-2 pb-10">
       {countries.countries.map((country, i) => (
         <ListItem key={i} country={country} />
       ))}
