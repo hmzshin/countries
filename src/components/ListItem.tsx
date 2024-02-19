2d5424b29eca256778566db7de1130a80a685109
hmzshin <hamzasahin@outlook.com>
2024-02-19 23:09:00 +0300

    fix type notations and add styling to component

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -2,11 +2,15 @@ import { useContext } from "react";
 import { useNavigate } from "react-router";
 import slugify from "slugify";
 import { CountriesContextObject } from "../context/countriesContext";
+type Object = {
+  name: string;
+};
 
 type Country = {
   name: string;
   code: string;
-  capital: string;
+  currencies: string[];
+  continent: Object;
 };
 
 interface ListItemProps {
@@ -45,19 +49,29 @@ const ListItem: React.FC<ListItemProps> = ({ country, randomColor }) => {
     }
   }
 
-  return (
-    <tr
+  return country ? (
+    <div
       id={country.code}
       className={`w-full flex justify-between items-center border cursor-pointer shadow-sm rounded-md p-2 ${
         active.code === country.code ? randomColor : "bg-gray-50"
       }`}
       onClick={() => clickHandler(country)}
     >
-      <td className="w-52 ">{country.name}</td>
-      <td className="w-20">{country.code}</td>
-      <td className="w-40"> {country.capital}</td>
+      <p className="w-52 ">{country.name}</p>
+      <p className="w-20 pl-2">{country.code}</p>
+      <p className="w-40 pl-7"> {country.continent.name}</p>
+      <p className="w-40 pl-10 flex flex-wrap">
+        {" "}
+        {country.currencies.map((currency, index) =>
+          country.currencies.length - 1 === index ? (
+            <span key={index}>{currency}</span>
+          ) : (
+            <span className="mr-1" key={index}>{`${currency},`}</span>
+          )
+        )}
+      </p>
 
-      <td onClick={() => navigateToDetails(country)}>
+      <p onClick={() => navigateToDetails(country)}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="1.5em"
@@ -70,8 +84,10 @@ const ListItem: React.FC<ListItemProps> = ({ country, randomColor }) => {
             d="M5.536 21.886a1.004 1.004 0 0 0 1.033-.064l13-9a1 1 0 0 0 0-1.644l-13-9A.998.998 0 0 0 5 3v18a1 1 0 0 0 .536.886M7 4.909L17.243 12L7 19.091z"
           />
         </svg>
-      </td>
-    </tr>
+      </p>
+    </div>
+  ) : (
+    <div>Loading</div>
   );
 };
 
